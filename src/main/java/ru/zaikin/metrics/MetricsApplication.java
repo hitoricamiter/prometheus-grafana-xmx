df1746f8c291fd6d439f4fd4de5dcc55100179fd
hitoricamiter <koshurnikovaslyam@mail.ru>
2025-07-18 11:04:25 +0600

    add metricbinders

--- src/main/java/ru/zaikin/metrics/MetricsApplication.java
+++ src/main/java/ru/zaikin/metrics/MetricsApplication.java
@@ -1,13 +1,32 @@
 package ru.zaikin.metrics;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.binder.MeterBinder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MetricsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MetricsApplication.class, args);
+	}
+
+	@Bean
+	MeterBinder meterBinder() {
+		return registry -> {
+			Counter.builder("greetings_count")
+					.description("Количество обращений")
+					.register(registry);
+
+			Counter.builder("greetings_count_by_name")
+					.description("Количество обращений по имени")
+					.tag("name", "user")
+					.register(registry);
+		};
+
+
 	}
 
 }
--- src/main/java/ru/zaikin/metrics/controller/GreetingsRestController.java
+++ src/main/java/ru/zaikin/metrics/controller/GreetingsRestController.java
@@ -1,5 +1,7 @@
 package ru.zaikin.metrics.controller;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,13 +9,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.zaikin.metrics.presentation.GreetingPresentationV1;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/greetings")
 public class GreetingsRestController {
 
+    MeterRegistry registry;
+
+    public GreetingsRestController(MeterRegistry registry) {
+        this.registry = registry;
+    }
+
     @GetMapping
     public ResponseEntity<GreetingPresentationV1> getGreeting(
             @RequestParam(name = "name", defaultValue = "user") String name) {
+
+        this.registry.counter("greetings_count", List.of()).increment();
+        this.registry.counter("greetings_count_by_name", List.of(Tag.of("name", name))).increment();
         return ResponseEntity.ok(new GreetingPresentationV1("Hello, %s!".formatted(name)));
     }
 
